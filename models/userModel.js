4a1c106ef04058f1c3d5ef0300fc588948161879
Sarah Lachiheb <sarah.lachiheb@gmail.com>
2021-01-29 11:29:04 +0100

    modeling users

---
+++ models/userModel.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  nom: {
+    type: String,
+    trim: true,
+    required: [true, 'Please tell us your name'],
+  },
+  email: {
+    type: String,
+    lowercase: true,
+    trim: true,
+    unique: true,
+    required: [true, 'Please provide your email'],
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  photo: String,
+  password: {
+    type: String,
+    trim: true,
+    required: [true, 'Please provide a password'],
+    minLength: [8, 'password must be above 1.0'],
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
