2c47695a0114369ae64137b9eb5dc522f53de400
Sarah Lachiheb <sarah.lachiheb@gmail.com>
2021-02-01 15:20:17 +0100

    sending emails with naodemailer

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -6,8 +6,10 @@ const router = express.Router();
 
 router.post('/signup', authController.signUp);
 router.post('/login', authController.login);
+
 router.post('/forgotPassword', authController.forgotPassword);
-router.post('/resetPassword', authController.resetPassword);
+router.patch('/resetPassword/:token', authController.resetPassword);
+
 router
   .route('/')
   .get(userController.getAllUsers)
