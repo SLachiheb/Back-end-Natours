b74885c5620ac596982f0954792917f2397e4f46
Sarah Lachiheb <sarah.lachiheb@gmail.com>
2021-02-04 14:50:32 +0100

    modelling tour guides : embedding

--- models/tourModel.js
+++ models/tourModel.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const slugify = require('slugify');
 // const validator = require('validator');
 
+const User = require('./userModel');
+
 const tourSchema = new mongoose.Schema(
   {
     name: {
@@ -103,6 +105,7 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
+    guides: Array,
   },
   {
     toJSON: { virtuals: true },
@@ -117,6 +120,14 @@ tourSchema.virtual('durationWeeks').get(function () {
 // DOCUMENT MIDDLEWARE: runs before .save() and create() commands
 tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+tourSchema.pre('save', async function (next) {
+  const guidesPromises = this.guides.map(
+    async guideID => await User.findById(guideID)
+  );
+  this.guides = await Promise.all(guidesPromises);
   next();
 });
 
