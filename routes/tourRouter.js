c20ad68447c4a1b041037cdc6f037cd3c73f2d66
Sarah Lachiheb <sarah.lachiheb@gmail.com>
2021-02-05 12:10:25 +0100

    implementing simple nested routes

--- routes/tourRouter.js
+++ routes/tourRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const tourController = require('../controllers/tourController');
 const authController = require('../controllers/authController');
+const reviewController = require('../controllers/reviewController');
 
 const router = express.Router();
 
@@ -26,6 +27,17 @@ router
     authController.protect,
     authController.restrictTo('admin', 'lead-guide'),
     tourController.deleteTour
+  );
+
+// Post api/v1/tour/idTour/reviews
+// Get api/v1/tour/idTour/reviews/idReview
+
+router
+  .route('/:tourId/reviews')
+  .post(
+    authController.protect,
+    authController.restrictTo('user'),
+    reviewController.createReview
   );
 
 module.exports = router;
