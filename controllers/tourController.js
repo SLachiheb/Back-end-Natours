3a9bc79ffe30df04b67f97da9236bbcfbd277490
Sarah Lachiheb <sarah.lachiheb@gmail.com>
2021-02-05 14:39:54 +0100

    factory functions: delete

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,4 +1,5 @@
 const Tour = require('../models/tourModel');
+const factory = require('./handlerFactory');
 const APIFeatures = require('../utils/apiFeatures');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
@@ -65,15 +66,16 @@ exports.updateTour = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.deleteTour = catchAsync(async (req, res, next) => {
-  const tour = await Tour.findByIdAndDelete(req.params.id);
+exports.deleteTour = factory.deleteOne(Tour);
+// exports.deleteTour = catchAsync(async (req, res, next) => {
+//   const tour = await Tour.findByIdAndDelete(req.params.id);
 
-  if (!tour) {
-    return next(new AppError('No tour find with that ID', 404));
-  }
+//   if (!tour) {
+//     return next(new AppError('No tour find with that ID', 404));
+//   }
 
-  res.status(204).json({ status: 'success', data: null });
-});
+//   res.status(204).json({ status: 'success', data: null });
+// });
 
 exports.getTourStats = catchAsync(async (req, res, next) => {
   const stats = await Tour.aggregate([
