07390880c896d18f6559a1e829968d37262f5938
Sarah Lachiheb <sarah.lachiheb@gmail.com>
2021-02-06 11:20:21 +0100

    /me endpoint

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -42,7 +42,9 @@ exports.createOne = Model =>
 exports.getOne = (Model, popOptions) =>
   catchAsync(async (req, res, next) => {
     let query = Model.findById(req.params.id);
-    if (popOptions) query = query.populate(popOptions);
+    if (popOptions) {
+      query = query.populate(popOptions);
+    }
     const doc = await query;
 
     if (!doc) {
--- controllers/userController.js
+++ controllers/userController.js
@@ -54,8 +54,12 @@ exports.createUser = (req, res) => {
   });
 };
 
+exports.getMe = (req, res, next) => {
+  req.params.id = req.user.id;
+  next();
+};
 exports.getAllUsers = factory.getAll(User);
-exports.getUser = factory.getOne(User, {});
+exports.getUser = factory.getOne(User);
 // Do NOT update password with this!
 exports.updateUser = factory.updateOne(User);
 exports.deleteUser = factory.deleteOne(User);
