e29c0b04722c5a80114cbcf8b1375f72acdc5121
Sarah Lachiheb <sarah.lachiheb@gmail.com>
2021-02-09 17:53:18 +0100

    geospatial queries: finding tours within radius

--- routes/tourRouter.js
+++ routes/tourRouter.js
@@ -34,6 +34,10 @@ router
   );
 
 router
+  .route('/tours-within/:distance/center/:latlng/unit/:unit')
+  .get(tourController.getToursWithin);
+
+router
   .route('/')
   .get(tourController.getAllTours)
   .post(
