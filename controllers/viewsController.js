045bddb6fb4e749f6e87eae1706450e313bd97f2
Sarah Lachiheb <sarah.lachiheb@gmail.com>
2021-02-11 17:50:00 +0100

    building the tour page - part 1

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,6 +1,5 @@
 const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
-const AppError = require('../utils/appError');
 
 exports.getOverview = catchAsync(async (req, res, next) => {
   // 1) Get tour data from collection
@@ -14,8 +13,17 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = (req, res) => {
+exports.getTour = catchAsync(async (req, res) => {
+  // 1) get the data, for the requested tour (including reviews ans guides)
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
+  });
+  console.log(tour);
+  // 2) Build template
+  // 3) Render that template using tour data from 1)
   res.status(200).render('tour', {
     title: 'The forest Hiker tour',
+    tour,
   });
-};
+});
