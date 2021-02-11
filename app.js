a5806e165093fe6d3f2451fda96dfb382047b906
Sarah Lachiheb <sarah.lachiheb@gmail.com>
2021-02-11 13:38:32 +0100

    extending our base template with blocks

--- app.js
+++ app.js
@@ -69,10 +69,22 @@ app.use((req, res, next) => {
 });
 
 // 3) ROUTES
-app.get('/', (req, res, next) => {
+app.get('/', (req, res) => {
   res.status('200').render('base', {
     tour: 'The Forest Hider',
     user: 'Sarah',
+  });
+});
+
+app.get('/overview', (req, res) => {
+  res.status(200).render('overview', {
+    title: 'All Tours',
+  });
+});
+
+app.get('/tour', (req, res) => {
+  res.status(200).render('tour', {
+    title: 'The forest Hiker tour',
   });
 });
 
