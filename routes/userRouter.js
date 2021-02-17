2b21779ce6325002794ef86fd466f99dc6928432
Sarah Lachiheb <sarah.lachiheb@gmail.com>
2021-02-17 10:51:58 +0100

    imgae upload using multer : users

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const multer = require('multer');
 const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
 
+const upload = multer({
+  dest: 'public/img/users',
+});
 const router = express.Router();
 
 router.post('/signup', authController.signUp);
@@ -14,7 +18,7 @@ router.patch('/resetPassword/:token', authController.resetPassword);
 router.use(authController.protect);
 
 router.get('/me', userController.getMe, userController.getUser);
-router.patch('/updateMe', userController.updateMe);
+router.patch('/updateMe', upload.single('photo'), userController.updateMe);
 router.delete('/deleteMe', userController.deleteMe);
 router.patch('/updateMyPassword', authController.updatePassword);
 
